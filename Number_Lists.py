031961014ca2052e255e2ab8a196f5151275c708
Jason-Mez <46197717+Jason-Mez@users.noreply.github.com>
2021-03-30 08:56:00 +0800

    Add files via upload

---
+++ Number_Lists.py
@@ -0,0 +1,28 @@
+even = [2, 4, 6, 8] 
+odd = [1, 5, 7, 9] 
+
+#Getting the minimum value 
+print(min(odd)) 
+print(min(even)) 
+print()
+#Getting the maximum values 
+print(max(even)) 
+print(max(odd)) 
+print() 
+#Getting the length of the string. 
+print(len(even)) 
+print(len(odd)) 
+
+
+#Using the .count() method. 
+#Use it to count specific amout of values in a system. 
+print(even.count(1))  # Returns an integer. 
+
+#String methods. 
+string = "Hello" 
+
+
+#Experimenting with mutable types 
+cars = ["Honda", "Ford", "Toyota", "Mazda"]  
+cars[0] = "La"  
+print(cars)
