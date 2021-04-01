8a8f756046e3ea49c6da83a130629793d024aad9
Jason-Mez <46197717+Jason-Mez@users.noreply.github.com>
2021-04-01 14:54:45 +0800

    Create List_Within_List.py

---
+++ Basic_Lists/List_Within_List.py
@@ -0,0 +1,9 @@
+#Create an empty
+empty_list = [] 
+#Creating an even list. 
+even = [2, 4 , 6, 8]
+#creating an odd list.
+odd = [1, 3, 5, 7]
+
+#This create a list within a list. 
+numbers = [even, odd]
