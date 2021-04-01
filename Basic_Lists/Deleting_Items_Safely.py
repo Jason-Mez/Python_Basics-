473e99647150ff954f91dcf69809a684eb26333e
Jason-Mez <46197717+Jason-Mez@users.noreply.github.com>
2021-04-01 08:51:46 +0800

    Create Deleting_Items_Safely.py

---
+++ Basic_Lists/Deleting_Items_Safely.py
@@ -0,0 +1,25 @@
+#Creating a data set
+data = [4, 5, 104, 105, 110, 120, 130, 130, 150,
+        160, 170, 183, 185, 187, 188, 191, 350, 360]
+
+# del data[0:2]
+# print(data)
+# del data[14:]
+# print(data)
+
+#Setting minimum value.
+min_valid = 100 
+#Setting the maximum value. 
+max_valid = 200
+#Setting a stop value 
+stop = 0
+
+#Process the low values.  
+for index, value in enumerate(data):  #This works for sorted lists.
+    if value >= min_valid: 
+        stop = index  #Note that no changes are being made to the list within for loop only in the outter for loop. 
+        break 
+
+print(stop) 
+del data[:stop] #This works for sorted lists. #Upto, not including.  
+print(data)
