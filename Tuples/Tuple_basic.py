c17f931af3343609bebecdb55219e4de67ef5ef0
Jason-Mez <46197717+Jason-Mez@users.noreply.github.com>
2021-04-05 16:14:43 +0800

    Add files via upload

---
+++ Tuples/Tuple_basic.py
@@ -0,0 +1,12 @@
+#Basic introduction to Tuples. 
+
+#Notation of a basic Tuple.  
+#You dont 
+t = "a", "b", "c" 
+print(type(t)) 
+
+#Using parenthesis. 
+x = "x", "y", "z" 
+print(type(x)) #Note how both of these are Tuples. 
+
+
