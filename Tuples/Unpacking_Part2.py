1fecca5bf53a348ee35335ef17662bd312da2c3f
Jason-Mez <46197717+Jason-Mez@users.noreply.github.com>
2021-04-06 09:45:09 +0800

    Create Unpacking_Part2.py
    Moral of the story: 
    You can put  Tuples in lists. -------> You can unpack tuples ( Ensure variable correspond with the length of the Tuple) ---------> You can loop through tuples in a list.

---
+++ Tuples/Unpacking_Part2.py
@@ -0,0 +1,12 @@
+#You can put Tuple in lists.
+albums = [("Welcome to my nightmare", "Alice Cooper", 1975),
+          ("Bad Comapany", "Bad Company", 1974),
+          ("Nightflight", "Budgie", 1981),
+          ("More Mayhem", "Emilda May", 2011),
+          ("Ride the lighting", "Metallica", 1984),
+          ]
+
+for album in albums: 
+    #You can unpack the tuple. 
+    title, artist, year = album
+    print(f"The title of the album is {title}, The artists is {artist}, The year is {year}")
