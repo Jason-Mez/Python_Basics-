e875fe72b4cd84a9c7298858b4423453b912b1bf
Jason-Mez <46197717+Jason-Mez@users.noreply.github.com>
2021-04-06 09:33:02 +0800

    Create unpacking.py

---
+++ Tuples/unpacking.py
@@ -0,0 +1,8 @@
+table = ("Coffee", 200, 100, 75, 34.50)
+
+#Here we are unpacking a Tuple.
+name, length, width, height, price = table
+
+#This allows us to break down the tuple.
+#Can make things easier to read.
+print(length * width)
