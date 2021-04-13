00ef75895fba45bcadc17201ad1c67fcf86c9843
Jason-Mez <46197717+Jason-Mez@users.noreply.github.com>
2021-04-13 09:04:32 +0800

    Update Palindrome.py

--- Functions/Palindrome.py
+++ Functions/Palindrome.py
@@ -1,1 +1,10 @@
-print("Hello World")
+#Script that shows if what you are typing in is a palindrom or not. 
+def palindrome(word):
+    return word[::-1] == word
+
+wording = input("Please type in the word : ")
+
+if palindrome(wording):
+    print(f"Yes, the word : {wording} is a palindrome.")
+else:
+    print(f"No, the word you typed is not a palindrome. ")
