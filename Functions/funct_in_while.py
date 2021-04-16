b9f7927b0de757b6585c53628c32ecc62323ca70
Jason-Mez <46197717+Jason-Mez@users.noreply.github.com>
2021-04-16 09:22:19 +0800

    Create funct_in_while.py

---
+++ Functions/funct_in_while.py
@@ -0,0 +1,30 @@
+import random
+
+def get_temp(prompt):  
+    while True:
+        temp = input(prompt) 
+        if temp.isnumeric(): 
+            return int(temp) 
+        else: 
+            print("Please enter a valid number.")
+
+
+highest = 1_000
+answer = random.randint(1, highest)
+print(answer)  # TODO: Remove after testing.
+guess = 0  # initialise to any number that doesn't equal answer
+print("Please guess a number between 1 and {}: ".format(highest))
+
+while guess != answer:
+    guess = get_temp(" : ")
+ 
+    if guess == 0:
+        break
+    if guess == answer:
+        print("Well done, you guessed it")
+        break
+    else:
+        if guess < answer:
+            print("Please guess higher")
+        else:  # guess must be greater than answer
+            print("Please guess lower")
