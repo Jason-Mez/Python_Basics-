bd59875fec9062666991afa539590f3f2aace8c5
Jason-Mez <46197717+Jason-Mez@users.noreply.github.com>
2021-04-19 20:43:16 +0800

    Create default_values.py

---
+++ Functions/default_values.py
@@ -0,0 +1,14 @@
+def banner(text = " ", screen_width= 80): 
+     
+    if len(text) > screen_width - 4 :
+        raise ValueError(f"The {text} is larger than the specified width {screen_width}") 
+
+    if text == "*": 
+        print("*" * screen_width) 
+    else:  
+        cetenred_text = text.center(screen_width - 4 )
+        output_string = f"**{cetenred_text}**"    
+        print(output_string) 
+
+
+banner(screen_width=80)
