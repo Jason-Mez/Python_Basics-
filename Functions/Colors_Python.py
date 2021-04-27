ebf29f88e7982ca02851af0836fe079bcc70fa88
Jason-Mez <46197717+Jason-Mez@users.noreply.github.com>
2021-04-27 11:59:47 +0800

    Update Colors_Python.py

--- Functions/Colors_Python.py
+++ Functions/Colors_Python.py
@@ -12,3 +12,8 @@ RESET = '\u001b[0m'
 BOLD = '\u001b[1m'
 UNDERLINE = '\u001b[4m'
 REVERSE = '\u001b[7m'
+
+#This is just basic examples, on how to print in color. 
+print(RED, "This is cool, i can print in color",RESET)
+print(BLUE, "What i can set this up in blue")
+print(GREEN ,"Yea, Now we i can finally add some \"color\" ")
